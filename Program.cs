a0e2d52ba75fab0d7aab0a9ece84174b15512e17
Chris <c_diaz86@hotmail.com>
2015-09-21 17:51:18 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CheckIfPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string input = Console.ReadLine();
+
+                char[] inputArray = input.ToCharArray();
+
+                string output = string.Empty;
+                for (int i = inputArray.Length - 1; i > -1; i--)
+                {
+                    output += inputArray[i];
+                }
+
+                var message = input == output ? "This is a palindrome!" : "This is NOT a palindrome";
+                Console.WriteLine(message);
+
+            } while (true);
+        }
+    }
+}
